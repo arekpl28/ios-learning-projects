105c26cf373987b156d482916e397874a6f15fcf
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-07-24 18:41:38 +0200

    Add Playground with test code

---
+++ SwiftCodePlayground.playground/Contents.swift
@@ -0,0 +1,72 @@
+import UIKit
+
+//var greeting:String = "Hello, playground"
+//print(greeting)
+//
+//greeting = "I can write Swift!"
+//print(greeting)
+//
+//var myInt:Int = 3
+//print(myInt)
+//
+//let myDouble:Double = 5.4
+//print(myDouble)
+//
+//var myBool:Bool = false
+//print(myBool)
+//
+////////////////////////
+///
+//func sayHello() {
+//    var greeting = "Hello, playground"
+//    print(greeting)
+//}
+//
+//sayHello()
+//
+//func sayHi(msg:String, num:Int)  {
+//    print(msg, num)
+//}
+//
+//sayHi(msg: "Hi, playground!", num: 34)
+///////////////////
+///
+//func doAdd(a:Int, b:Int){
+//    var sum = a + b
+//    print(sum)
+//
+//}
+//doAdd(a: 2, b: 3)
+///////////
+//func doAdd2(a:Int, b:Int) -> Int {
+//    var sum:Int = a+b
+//    return sum
+//}
+//
+//var result:Int = doAdd2(a: 4, b: 5)
+//print(result)
+/////////////
+//func doAdd3(firstNum a:Int, secondNum b:Int) -> Int {
+//    var sum:Int = a+b
+//    return sum
+//}
+//
+//var result3:Int = doAdd3(firstNum: 4, secondNum: 4)
+//print(result3)
+/////////////
+//func doAdd4(_ a:Int, _ b:Int) -> Int {
+//    var sum:Int = a+b
+//    return sum
+//}
+//
+//var result4:Int = doAdd4(8, 8)
+//print(result4)
+
+var a = 4
+var b = 5
+
+if a > b {
+    print("OK")
+} else if b > a {
+    print("KO")
+}
