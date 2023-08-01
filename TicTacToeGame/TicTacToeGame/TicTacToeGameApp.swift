524f211aebe1fdfd896add0fbf2bc704e2d916b7
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-01 21:05:41 +0200

    Add assets and launch screen

---
+++ TicTacToeGame/TicTacToeGame/TicTacToeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeGameApp.swift
+//  TicTacToeGame
+//
+//  Created by Arkadiusz Plumbaum on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
