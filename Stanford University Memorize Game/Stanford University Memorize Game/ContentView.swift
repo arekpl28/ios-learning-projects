fec9f61a8f2745371234fe6ed0b89336bc3b4bed
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-04 21:10:56 +0200

    Add memory card game UI with card display and buttons
    This commit adds the initial UI for the memory card game. It includes a VStack with horizontally displayed cards, each represented by the CardView. The UI also provides buttons to add or remove cards from the game board.
    - ContentView: Contains the main game UI logic and components.
    - CardView: Represents an individual card with its content and flip functionality.

---
+++ Stanford University Memorize Game/Stanford University Memorize Game/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  Stanford University Memorize Game
+//
+//  Created by Arkadiusz Plumbaum on 03/08/2023.
+//
+
+// This is the main view for the game.
+// It contains the UI elements and logic for the card matching game.
+
+import SwiftUI
+
+struct ContentView: View {
+    // Array of emojis used as card content
+    var emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎️", "🚑", "🚓", "🚒", "✈️", "🚀", "🚢", "🛸", "🛶", "🚌", "🏍️", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🚛", "🛴"]
+    
+    // State variable to control the number of visible cards
+    @State var emojiCount = 4
+    
+    var body: some View {
+        VStack {
+            // Display the cards in a horizontal row
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji) // Display each card using the CardView
+                }
+            }
+            Spacer()
+            
+            // UI for adding or removing cards
+            HStack {
+                remove
+                Spacer ()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        // Button to remove a card
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        // Button to add a card
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+}
+
+// CardView is a custom View to represent a card
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            
+            if isFaceUp {
+                // Display the front side of the card
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                // Display the back side of the card
+                shape.fill()
+                shape.stroke(lineWidth: 3)
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp // Toggle card face
+        }
+    }
+}
+
+// PreviewProvider for ContentView
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stanford University Memorize Game/Stanford University Memorize Game/Stanford_University_Memorize_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_University_Memorize_GameApp.swift
+//  Stanford University Memorize Game
+//
+//  Created by Arkadiusz Plumbaum on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_University_Memorize_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
