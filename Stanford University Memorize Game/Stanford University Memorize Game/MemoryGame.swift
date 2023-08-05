242d6119ff82a8cdd221064b162456e7286d2c78
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-05 18:49:49 +0200

    Refactor MemoryGame initialization and encapsulation
    This commit refactors the MemoryGame structure by encapsulating the `cards` property with a private(set) access modifier, ensuring that it can be read but not modified directly from outside the structure.
    The initialization process is improved with the introduction of the init method that creates a specified number of pairs of cards using the provided card content creation function. Additionally, each card is assigned a unique identifier using the `Identifiable` protocol.

--- Stanford University Memorize Game/Stanford University Memorize Game/MemoryGame.swift
+++ Stanford University Memorize Game/Stanford University Memorize Game/MemoryGame.swift
@@ -22,15 +22,16 @@ struct MemoryGame<CardContent> {
         // Add numberOfPairsOfCards X 2 cards to the cards array
         for pairIndex in 0..<numberOfPairsOfCards {
             let content: CardContent = createCardContent(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
         }
     }
     
     // The internal Card structure describes card properties.
-    struct Card {
-        var isFaceUp: Bool = false
+    struct Card: Identifiable {
+        var isFaceUp: Bool = true
         var isMatched: Bool = false
         var content: CardContent
+        var id: Int
     }
 }
