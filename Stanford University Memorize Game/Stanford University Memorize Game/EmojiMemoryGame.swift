5e3f63e5c3c65b99e0fb15e6ed3c971948f0fc72
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-05 18:23:12 +0200

    Implement EmojiMemoryGame class
    This commit introduces the `EmojiMemoryGame` class, responsible for managing the game's main logic and data. It provides a static function to create a new instance of `MemoryGame` using a predefined set of emojis as card content.

---
+++ Stanford University Memorize Game/Stanford University Memorize Game/EmojiMemoryGame.swift
@@ -0,0 +1,29 @@
+//
+//  EmojiMemoryGame.swift
+//  Stanford University Memorize Game
+//
+//  Created by Arkadiusz Plumbaum on 05/08/2023.
+//
+
+import SwiftUI
+
+// The EmojiMemoryGame class defines the main game logic and data.
+class EmojiMemoryGame {
+    // A static array of emojis used as card content
+    static let emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎️", "🚑", "🚓", "🚒", "✈️", "🚀", "🚢", "🛸", "🛶", "🚌", "🏍️", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🚛", "🛴"]
+    
+    // A static function to create a new MemoryGame instance
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    // Private instance of MemoryGame created using the createMemoryGame function
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    // Expose the array of cards from the model
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+}
