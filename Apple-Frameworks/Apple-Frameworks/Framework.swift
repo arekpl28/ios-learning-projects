f726da51d35dd9e2dd68499fe5a51f3ac1fe1034
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-08 19:35:53 +0200

    Added FrameworkGridView with grid layout and title view

--- Apple-Frameworks/Apple-Frameworks/Framework.swift
+++ Apple-Frameworks/Apple-Frameworks/Framework.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 // Data structure for representing a framework
-struct Framework {
+struct Framework: Hashable, Identifiable {
+    let id = UUID()
     let name: String
     let imageName: String
     let urlString: String
--- Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
@@ -8,10 +8,57 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    // Define grid columns
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                // Create a LazyVGrid with defined columns
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) {framework in
+                        // Display each framework's title using FrameworkTitleView
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks") // Navigation title
+            
+        }
     }
 }
+
+
+
+struct FrameworkTitleView: View {
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            // Display framework image
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            // Display framework name
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
+
+
+
+
+
+
+
+
 
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
