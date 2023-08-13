85df9927babcb6546ccd2ce446256170b23f2676
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-13 17:43:03 +0200

    Added BarcodeScanner view

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Arkadiusz Plumbaum on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
