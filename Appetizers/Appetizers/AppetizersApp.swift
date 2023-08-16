fedefd896e186d63877ad0e373367407bf532738
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-16 10:37:49 +0200

    Create main App struct and set up initial window with AppetizerTabView

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Arkadiusz Plumbaum on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetizerTabView()
+        }
+    }
+}
