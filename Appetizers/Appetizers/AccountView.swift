6af46b4b9c8d0aeb759e04413d1c1c0501cf3860
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-16 10:36:50 +0200

    Add initial AccountView implementation

---
+++ Appetizers/Appetizers/AccountView.swift
@@ -0,0 +1,24 @@
+//
+//  AccountView.swift
+//  Appetizers
+//
+//  Created by Arkadiusz Plumbaum on 16/08/2023.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationView {
+            Text("Account View")
+                .navigationTitle("🤣 Account")
+        }
+       
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
