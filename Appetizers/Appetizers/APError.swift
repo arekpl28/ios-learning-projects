aa681a07785138502b8bc35b0459b82d82437758
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-17 09:36:58 +0200

    Add APError enum for API error handling

---
+++ Appetizers/Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Arkadiusz Plumbaum on 17/08/2023.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
