0eb55329b9ffbdcbdd2d9d5352eee03f11660162
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-17 09:37:28 +0200

    Add AppetizerListViewModel for managing appetizer data

---
+++ Appetizers/Appetizers/AppetizerListViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  AppetizerListViewModel.swift
+//  Appetizers
+//
+//  Created by Arkadiusz Plumbaum on 17/08/2023.
+//
+
+import Foundation
+
+final class AppetizerListViewModel: ObservableObject {
+    
+    @Published var appetizers: [Appetizer] = []
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
