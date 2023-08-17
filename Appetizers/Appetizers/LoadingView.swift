3053f9de99a68323f89027fbc2f0c92e7aaf3504
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-17 10:35:15 +0200

    Add LoadingView

---
+++ Appetizers/Appetizers/LoadingView.swift
@@ -0,0 +1,30 @@
+//
+//  LoadingView.swift
+//  Appetizers
+//
+//  Created by Arkadiusz Plumbaum on 17/08/2023.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let activityIndicatorView = UIActivityIndicatorView(style: .large)
+        activityIndicatorView.color = .brandPrimary
+        activityIndicatorView.startAnimating()
+        return activityIndicatorView
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {}
+}
+
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .edgesIgnoringSafeArea(.all)
+            
+            ActivityIndicator()
+        }
+    }
+}
