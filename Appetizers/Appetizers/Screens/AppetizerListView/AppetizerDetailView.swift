4a00aa811528fa2b41679a2fc3d2caaef8a7a79a
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-21 11:56:22 +0200

    Refactor nutritional information in AppetizerDetailView

--- Appetizers/Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
+++ Appetizers/Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -30,36 +30,9 @@ struct AppetizerDetailView: View {
                     .padding()
                 
                 HStack(spacing: 40) {
-                    
-                    VStack(spacing: 5) {
-                        Text("Calories")
-                            .bold()
-                            .font(.caption)
-                        Text(String(appetizer.calories))
-                            .foregroundColor(.gray)
-                            .fontWeight(.semibold)
-                            .italic()
-                    }
-                    
-                    VStack(spacing: 5) {
-                        Text("Carbs")
-                            .bold()
-                            .font(.caption)
-                        Text("\(appetizer.carbs) g")
-                            .foregroundColor(.gray)
-                            .fontWeight(.semibold)
-                            .italic()
-                    }
-                    
-                    VStack(spacing: 5) {
-                        Text("Protein")
-                            .bold()
-                            .font(.caption)
-                        Text("\(appetizer.protein) g")
-                            .foregroundColor(.gray)
-                            .fontWeight(.semibold)
-                            .italic()
-                    }
+                    NutritionInfo(title: "Calories", value: appetizer.calories)
+                    NutritionInfo(title: "Carbs", value: appetizer.carbs)
+                    NutritionInfo(title: "Protein", value: appetizer.protein)
                 }
             }
             
@@ -68,14 +41,7 @@ struct AppetizerDetailView: View {
             Button {
                 print("ok")
             } label: {
-                Text("$\(appetizer.price, specifier: "%.2f") - Add To Order")
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                    .frame(width: 260, height: 50)
-                    .foregroundColor(.white)
-                    .background(Color.brandPrimary)
-                    .cornerRadius(10)
-                
+                APButton(title: "$\(appetizer.price, specifier: "%.2f") - Add To Order")
             }
             .padding(.bottom, 30)
         }
@@ -86,17 +52,7 @@ struct AppetizerDetailView: View {
         .overlay(Button {
             isShowingDetail = false
         } label: {
-            ZStack {
-                Circle()
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(.white)
-                    .opacity(0.6)
-                
-                Image(systemName: "xmark")
-                    .imageScale(.small)
-                    .frame(width: 44, height: 44)
-                    .foregroundColor(.black)
-            }
+            XDismissButton()
         }, alignment: .topTrailing)
     }
 }
@@ -104,5 +60,24 @@ struct AppetizerDetailView: View {
 struct AppetizerDetailView_Previews: PreviewProvider {
     static var previews: some View {
         AppetizerDetailView(appetizer: MockData.sampleAppetizer, isShowingDetail: .constant(true))
+    }
+}
+
+struct NutritionInfo: View {
+    
+    let title: String
+    let value: Int
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            Text(title)
+                .bold()
+                .font(.caption)
+            
+            Text(String(value))
+                .foregroundColor(.gray)
+                .fontWeight(.semibold)
+                .italic()
+        }
     }
 }
