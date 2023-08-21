f9d054f1270951f19821accd8a4056ba07c129c9
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-21 10:57:53 +0200

    Add AppetizerDetailView for displaying details of an appetizer

---
+++ Appetizers/Appetizers/AppetizerDetailView.swift
@@ -0,0 +1,107 @@
+//
+//  AppetizerDetailView.swift
+//  Appetizers
+//
+//  Created by Arkadiusz Plumbaum on 21/08/2023.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    
+    let appetizer: Appetizer
+    var body: some View {
+        
+        VStack {
+            Image("asian-flank-steak")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 225)
+            
+            VStack {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Text(appetizer.description)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    .padding()
+                
+                HStack(spacing: 40) {
+                    
+                    VStack(spacing: 5) {
+                        Text("Calories")
+                            .bold()
+                            .font(.caption)
+                        Text(String(appetizer.calories))
+                            .foregroundColor(.gray)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack(spacing: 5) {
+                        Text("Carbs")
+                            .bold()
+                            .font(.caption)
+                        Text("\(appetizer.carbs) g")
+                            .foregroundColor(.gray)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack(spacing: 5) {
+                        Text("Protein")
+                            .bold()
+                            .font(.caption)
+                        Text("\(appetizer.protein) g")
+                            .foregroundColor(.gray)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            Button {
+                print("ok")
+            } label: {
+                Text("$\(appetizer.price, specifier: "%.2f") - Add To Order")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .frame(width: 260, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.brandPrimary)
+                    .cornerRadius(10)
+                
+            }
+            .padding(.bottom, 30)
+        }
+        .frame(width: 300, height: 525)
+        .background(Color(.systemBackground))
+        .cornerRadius(12)
+        .shadow(radius: 40)
+        .overlay(Button {
+            print("dismiss")
+        } label: {
+            ZStack {
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.white)
+                    .opacity(0.6)
+                
+                Image(systemName: "xmark")
+                    .imageScale(.small)
+                    .frame(width: 44, height: 44)
+                    .foregroundColor(.black)
+            }
+        }, alignment: .topTrailing)
+    }
+}
+
+struct AppetizerDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerDetailView(appetizer: MockData.sampleAppetizer)
+    }
+}
