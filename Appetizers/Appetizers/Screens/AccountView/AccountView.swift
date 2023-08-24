6f142e4e807ef0c75d08e54164bdef6ed75be80d
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-24 19:09:55 +0200

    Add AccountView for user info editing

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationView {
-            Text("Account View")
-                .navigationTitle("🤣 Account")
+            Form {
+                Section(header: Text("Personal info")) {
+                    TextField("First Name", text: $firstName)
+                        .textInputAutocapitalization(.words)
+                    TextField("Last Name", text: $lastName)
+                        .textInputAutocapitalization(.words)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled()
+                    DatePicker("Birthday", selection: $birthdate,
+                               displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+                
+                Section(header: Text("Request")){
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+            }
+            .navigationTitle("🤣 Account")
         }
-       
     }
 }
 
