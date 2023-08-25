536b5ade18de3ee4e8448382d61d00f839270a73
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-25 13:19:17 +0200

    Updated AccountView to use the ViewModel for user data management and added saving and retrieving user data functionality.

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -9,42 +9,45 @@ import SwiftUI
 
 struct AccountView: View {
     
-    @State private var firstName = ""
-    @State private var lastName = ""
-    @State private var email = ""
-    @State private var birthdate = Date()
-    @State private var extraNapkins = false
-    @State private var frequentRefills = false
+    @StateObject var viewModel = AccountViewModel()
     
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text("Personal info")) {
-                    TextField("First Name", text: $firstName)
+                    TextField("First Name", text: $viewModel.user.firstName)
                         .textInputAutocapitalization(.words)
-                    TextField("Last Name", text: $lastName)
+                    TextField("Last Name", text: $viewModel.user.lastName)
                         .textInputAutocapitalization(.words)
-                    TextField("Email", text: $email)
+                    TextField("Email", text: $viewModel.user.email)
                         .keyboardType(.emailAddress)
                         .textInputAutocapitalization(.never)
                         .autocorrectionDisabled()
-                    DatePicker("Birthday", selection: $birthdate,
+                    DatePicker("Birthday", selection: $viewModel.user.birthdate,
                                displayedComponents: .date)
                     
                     Button {
-                        print("Save")
+                        viewModel.saveChanges()
                     } label: {
                         Text("Save Changes")
                     }
                 }
                 
                 Section(header: Text("Request")){
-                    Toggle("Extra Napkins", isOn: $extraNapkins)
-                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                    Toggle("Extra Napkins", isOn: $viewModel.user.extraNapkins)
+                    Toggle("Frequent Refills", isOn: $viewModel.user.frequentRefills)
                 }
                 .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
             }
             .navigationTitle("🤣 Account")
+        }
+        .onAppear {
+            viewModel.retrieveUser()
+        }
+        .alert(item: $viewModel.alertItem) { alertItem in
+            Alert(title: alertItem.title,
+                  message: alertItem.message,
+                  dismissButton: alertItem.dismissButton)
         }
     }
 }
