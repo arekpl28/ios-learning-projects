2c411931b3202c49cf8344a3a5eeb807abc4c001
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-26 16:26:58 +0200

    Refactored the AccountView to include commented-out code for handling form field focus. Cleaned up and improved code readability.

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -10,16 +10,32 @@ import SwiftUI
 struct AccountView: View {
     
     @StateObject var viewModel = AccountViewModel()
+    //    @FocusState private var focusedTextField: FormTextField?
+    //
+    //    enum FormTextField {
+    //        case firstName, lastName, email
+    //    }
     
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text("Personal info")) {
                     TextField("First Name", text: $viewModel.user.firstName)
+                    //                        .focused($focusedTextField, equals: .firstName)
+                    //                        .onSubmit { focusedTextField = .lastName }
+                    //                        .submitLabel(.next)
                         .textInputAutocapitalization(.words)
+                    
                     TextField("Last Name", text: $viewModel.user.lastName)
+                    //                        .focused($focusedTextField, equals: .lastName)
+                    //                        .onSubmit { focusedTextField = .email }
+                    //                        .submitLabel(.next)
                         .textInputAutocapitalization(.words)
+                    
                     TextField("Email", text: $viewModel.user.email)
+                    //                        .focused($focusedTextField, equals: .email)
+                    //                        .onSubmit { focusedTextField = nil }
+                    //                        .submitLabel(.continue)
                         .keyboardType(.emailAddress)
                         .textInputAutocapitalization(.never)
                         .autocorrectionDisabled()
