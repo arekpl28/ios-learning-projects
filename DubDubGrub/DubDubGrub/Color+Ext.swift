f0bb7db5454e7b6eb12759e3fa81eee64df8fda8
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-30 20:22:19 +0200

    "Add Color Extension for brandPrimary in SwiftUI"

---
+++ DubDubGrub/DubDubGrub/Color+Ext.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Ext.swift
+//  DubDubGrub
+//
+//  Created by Arkadiusz Plumbaum on 30/08/2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
