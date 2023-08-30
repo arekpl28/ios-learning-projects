86c641a90e5a4307a60166b2ad37765f0e49ba33
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-30 19:05:33 +0200

    Initial commit

---
+++ DubDubGrub/DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Arkadiusz Plumbaum on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
