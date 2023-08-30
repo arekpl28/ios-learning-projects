5db6d806b79e24a06e816f78b3db5f517a04f460
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-30 21:04:20 +0200

    Create LocationListView.swift

---
+++ DubDubGrub/DubDubGrub/LocationListView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationListView.swift
+//  DubDubGrub
+//
+//  Created by Arkadiusz Plumbaum on 30/08/2023.
+//
+
+import SwiftUI
+
+struct LocationListView: View {
+    var body: some View {
+        Text("Location view")
+    }
+}
+
+struct LocationListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationListView()
+    }
+}
