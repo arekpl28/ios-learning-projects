ed9c6e581e859f95e8f8a3b1bce75f664fda8174
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-30 21:03:01 +0200

    Create AppTabView with tabs for Map, Location, and Profile

---
+++ DubDubGrub/DubDubGrub/AppTabView.swift
@@ -0,0 +1,37 @@
+//
+//  AppTabView.swift
+//  DubDubGrub
+//
+//  Created by Arkadiusz Plumbaum on 30/08/2023.
+//
+
+import SwiftUI
+
+struct AppTabView: View {
+    var body: some View {
+        TabView {
+            LocationMapView()
+                .tabItem {
+                    Label("Map", systemImage: "map")
+                        .environment(\.symbolVariants, .none)
+                }
+            LocationListView()
+                .tabItem {
+                    Label("Location", systemImage: "building")
+                        .environment(\.symbolVariants, .none)
+                }
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                        .environment(\.symbolVariants, .none)
+                }
+        }
+        .tint(.brandPrimary)
+    }
+}
+
+struct AppTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppTabView()
+    }
+}
