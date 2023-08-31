cd1cdc2f8b22a9a04647cf1c7854ddc9d01f22e6
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-08-31 10:47:48 +0200

    Implement LocationMapView with a Map and Logo

--- DubDubGrub/DubDubGrub/LocationMapView.swift
+++ DubDubGrub/DubDubGrub/LocationMapView.swift
@@ -6,10 +6,29 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516,
+                                                                                  longitude: -121.891054),
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.01,
+                                                                          longitudeDelta: 0.01))
     var body: some View {
-        Text("Map view")
+        ZStack {
+            Map(coordinateRegion: $region)
+                .edgesIgnoringSafeArea(.top)
+            
+            VStack {
+                Image("ddg-map-logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                .frame(height: 70)
+                .shadow(radius: 10)
+                
+                Spacer ()
+            }
+        }
     }
 }
 
