0c8d2a2c2f7e47a3ab0b8133c06ed59f80555b31
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-09-03 20:14:28 +0200

    Define RecordType enum for CKRecord types

---
+++ DubDubGrub/DubDubGrub/Utilties/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  DubDubGrub
+//
+//  Created by Arkadiusz Plumbaum on 03/09/2023.
+//
+
+import Foundation
+
+enum RecordType {
+    static let location = "DDGLocation"
+    static let profile = "DDGProfile"
+}
