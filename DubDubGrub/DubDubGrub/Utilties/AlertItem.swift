f79cdd38c176c86abd55b33cfa6c499ff81f69cd
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-09-13 17:44:52 +0200

    Updated location-related alert messages in AlertItem.swift

--- DubDubGrub/DubDubGrub/Utilties/AlertItem.swift
+++ DubDubGrub/DubDubGrub/Utilties/AlertItem.swift
@@ -21,4 +21,25 @@ struct AlertContext {
                                                                 Please try again.
                                                                 """),
                                                 dismissButton: .default(Text("OK")))
+    
+    static let locationRestricted   = AlertItem(title: Text("Locations Restricted"),
+                                                message: Text("""
+                                                                Your location is restricted.
+                                                                This may be due to parental controls.
+                                                                """),
+                                                dismissButton: .default(Text("OK")))
+    
+    static let locationDenied       = AlertItem(title: Text("Locations Denied"),
+                                                message: Text("""
+                                                                Dub Dub Grub does no have permission to access your location.
+                                                                To change that go to your phone's Settings > Dub Dub Grub > Location
+                                                                """),
+                                                dismissButton: .default(Text("OK")))
+    
+    static let locationDisabled     = AlertItem(title: Text("Locations Disabled"),
+                                                message: Text("""
+                                                                Your phone's location services are disabled.
+                                                                To change that go to your phone's Settings > Privacy > Location Services
+                                                                """),
+                                                dismissButton: .default(Text("OK")))
 }
