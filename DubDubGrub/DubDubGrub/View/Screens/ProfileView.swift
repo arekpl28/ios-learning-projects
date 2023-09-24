162996250995f55d1abc04c40cbd893f7104d1da
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-09-24 19:52:58 +0200

    Updated ProfileView to include the ability to select an avatar image and improved UI for profile information

--- DubDubGrub/DubDubGrub/View/Screens/ProfileView.swift
+++ DubDubGrub/DubDubGrub/View/Screens/ProfileView.swift
@@ -13,6 +13,8 @@ struct ProfileView: View {
     @State private var lastName     = ""
     @State private var companyName  = ""
     @State private var bio          = ""
+    @State private var avatar       = PlaceholderImage.avatar
+    @State private var isShowingPhotoPicker = false
     
     var body: some View {
         VStack {
@@ -21,9 +23,13 @@ struct ProfileView: View {
                 
                 HStack(spacing: 16) {
                     ZStack {
-                        AvatarView(size: 84)
+                        AvatarView(image: avatar, size: 84)
                         EditImage()
-                    }.padding(.leading, 12)
+                    }
+                    .padding(.leading, 12)
+                    .onTapGesture {
+                        isShowingPhotoPicker = true
+                    }
                     
                     VStack(spacing: 1) {
                         TextField("First Name", text: $firstName)
@@ -53,7 +59,11 @@ struct ProfileView: View {
             } label: {
                 DDGButton(title: "Create Profile")
             }
-        }.navigationTitle("Profile")
+        }
+        .navigationTitle("Profile")
+        .sheet(isPresented: $isShowingPhotoPicker) {
+            PhotoPicker(image: $avatar)
+        }
     }
 }
 
